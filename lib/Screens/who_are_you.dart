5687a173b6f3181d30d600db9b0c396dda9051f8
AhmedYasee <eng.ahmedyaseen4@gmail.com>
2024-03-01 20:17:08 +0200

    SmoothPageIndicator

--- lib/Screens/who_are_you.dart
+++ lib/Screens/who_are_you.dart
@@ -1,1 +1,100 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
+class WHoAreYou extends StatelessWidget {
+  const WHoAreYou({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(
+          children: [
+            Container(
+                height: 500,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(180),
+                      bottomRight: Radius.circular(180)),
+                  color: Color.fromARGB(255, 248, 245, 245),
+                )),
+            Positioned(
+              bottom: 50,
+              right: 30,
+              left: 30,
+              child: Image.asset(
+                'assets/images/aaa.png',
+                width: 500.0,
+                height: 300,
+                fit: BoxFit.contain,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: Container(
+                alignment: Alignment.center,
+                child: const Text(
+                  'Welcome To',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Image.asset(
+          'assets/images/Book.jpg',
+          fit: BoxFit.fill,
+          width: 350,
+          height: 100,
+        ),
+        const SizedBox(
+          height: 50,
+        ),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const WHoAreYou()));
+          },
+          child: Container(
+            margin: EdgeInsets.all(25),
+            width: double.infinity,
+            height: 70,
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 1,
+                color: Color(0xff2c53b7),
+              ),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  textAlign: TextAlign.center,
+                  'Let\'s Go',
+                  style: TextStyle(
+                      fontSize: 28,
+                      color: Color(0xff2c53b7),
+                      fontWeight: FontWeight.bold),
+                ),
+                Icon(
+                  Icons.arrow_forward_ios_outlined,
+                  color: Color(0xff2c53b7),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:fluttertest/Screens/regestration_one.dart';
 import 'package:fluttertest/Screens/welcomescreen.dart';
+import 'package:fluttertest/Screens/who_are_you.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+PageController controller = new PageController();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -17,10 +22,22 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(scaffoldBackgroundColor: Colors.white),
         home: SafeArea(
           child: Scaffold(
-            body: PageView(
-              children: const [
-                welcomescreen(),
-                Regestration(),
+            body: Stack(
+              children: [
+                PageView(controller: controller, children: [
+                  welcomescreen(),
+                  Regestration(),
+                  WHoAreYou(),
+                ]),
+                Container(
+                    alignment: Alignment(0, .95),
+                    child: SmoothPageIndicator(
+                      controller: controller,
+                      count: 3,
+                      effect: WormEffect(
+                        activeDotColor: Color(0xff2c53b7),
+                      ),
+                    )),
               ],
             ),
           ),
